cde99e853e084f03008271d795e696cf35b21559
suyeonchooo <chosy080709@gmail.com>
2024-10-20 22:25:05 +0900

    Updated all

--- run/model.py
+++ run/model.py
@@ -1,29 +1,95 @@
 import torch
 import numpy as np
 import torch.nn as nn
-import torch.nn.functional as F
 
-def embedding_layer(vocab_size, embedding_size):
-    embedding = nn.Embedding(vocab_size, embedding_size)
-    
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+labels = ["0", "1"]
+predictions = []
 
 class RNNModel():
-    def __init__ (self, hidden_size=512, output_size=2, time_steps=0):
-        self.hidden_size = hidden_size
-        self.output_size = output_size            # o(negetive) or 1(positive)
-        self.time_steps = time_steps
+    def __init__(self, input):
+        self.input = input.to(device)
+
+        # Size
+        # input 구성(batch_size, sequence_length, feature_size) 
+        self.batch_size = self.input.size()[0]
+        self.sequence_length = self.input.size()[1]
+        self.input_size = self.input.size()[2]
+
+        self.hidden_size = 512
+        self.output_size = 2                              # o(negetive) or 1(positive)
 
         # Weight Initialization
-        self.W_hh = torch.randn(self.hidden_size, self.hidden_size)
-        self.W_xh = torch.randn(self.input_size, self.hidden_size)
-        self.W_hy = torch.randn(self.hidden_size, self.output_size)
+        self.W_hh = torch.randn(self.hidden_size, self.hidden_size, device=device)
+        self.W_xh = torch.randn(self.input_size, self.hidden_size, device=device)
+        self.W_hy = torch.randn(self.hidden_size, self.output_size, device=device)
 
-    def init_hidden_state(self):
-        return torch.zeros(1, self.hidden_size)
+        # print(f'input: {self.input.size()}')
+        # print(f'input size: {self.input_size}')
+        # print(f'batch size: {self.batch_size}')
+        # print(f'sequence length: {self.sequence_length}')
+        # print(f'hidden size: {self.hidden_size}')
+        # print(f'output size: {self.output_size}')
 
-    def forward (self, input):
-        x = self.embedding(input)
-        h_0 = self.init_hidden_state()
+        # # print(f'W_hh: {self.W_hh}')
+        # print(f'W_hh size: {self.W_hh.size()}')
+        # # print(f'W_xh: {self.W_xh}')
+        # print(f'W_xh size: {self.W_xh.size()}')
+        # # print(f'W_hy: {self.W_hy}')
+        # print(f'W_hy size: {self.W_hy.size()}')
+    
+    def forward(self):
+        hidden_state = torch.zeros(self.batch_size, self.hidden_size).to(device)        # 초기 hidden state
 
-    def backward (self, l, y):
+        for t in range(self.sequence_length):
+            x_t = self.input[:, t, :]
+
+            h = torch.matmul(hidden_state, self.W_hh)
+            x = torch.matmul(x_t, self.W_xh)
+            updated_h = torch.tanh(h + x)
+
+            hidden_state = updated_h                                                    # hidden state update           
+
+        logits = torch.matmul(updated_h, self.W_hy)
+        print(logits)
+        print(logits.size())
+        return logits
+    
+    def binary_cross_entropy_loss(self, pred, labels):
+        loss = torch.tensor(0.0, device=device)
+        # labels 구성(batch_size, label_size)
+        for batch in range(self.batch_size):
+            label = torch.tensor(float(labels[batch]), device=device)
+            label_index = int(labels[batch])
+            print(label)
+            print(label_index)
+            print(pred[batch][label_index])
+            loss += label * torch.log(pred[batch][label_index]) + (1 - label) * torch.log(1- pred[batch][label_index])
+            print(loss)
+        average_loss = -(loss / self.batch_size)
+
+        return average_loss
+       
+    def backward (self, learning_rate, loss):
         pass
+
+    def optimizer(self):
+        pass
+    
+    def output_layer(self, logits):
+        pass
+
+
+    # def prediction (self, updated_h):
+    #     logits = torch.matmul(updated_h, self.W_hy)
+
+    #     softmax = nn.Softmax(dim=1)
+    #     y = softmax(logits)
+
+    #     pred_index = torch.argmax(y)
+    #     pred = labels[pred_index.item()]               # pred_index를 정수로 변환
+    #                                                    # torch.argmax()는 tensor로 결과를 반환 > .item()이 tensor에서 scalar 값을 추출해줌
+    #                                                    # (tensor(1)에서 1이라는 값을 빼내는 역할을 하는 것)
+    #     predictions.append(pred)
+    #     return predictions
---
+++ run/train.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+# 현재 파일의 경로 기준으로 상위 디렉터리(RNN 폴더 경로) 추가
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import data         # RNN 폴더 안의 data.py 모듈을 가져오기
+import model
+
+EPOCHS = 1
+for epoch in range(EPOCHS):
+    # for batch in range(data.batch_size):
+    for batch in range(1):
+        # print(len(data.sst_embeddings))
+        # print(len(data.sst_embeddings[batch]))
+        myRNN = model.RNNModel(data.sst_embeddings[batch])
+
+        prediction = myRNN.forward()
+        print("The forward pass was successful.")
+
+        loss = myRNN.binary_cross_entropy_loss(prediction, data.sst_labels)
+        print("The loss was successfully obtained.")
+        print(f'loss: {loss}')
+
+        # pred = myRNN.prediction(h)
